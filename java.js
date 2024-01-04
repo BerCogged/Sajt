19c9f3a9211120ac77c5beb5714c932226b7f9ed
BerCogged <bercogged@gmail.com>
2024-01-04 17:42:05 +0100

    Roullete

--- java.js
+++ java.js
@@ -5,7 +5,7 @@ document.getElementById("drugo_dugme").addEventListener('click',pokazi_drugog);
 function pomeranje(){
     let t = document.getElementById("desni_nav");
     t.style.width = '20%';
-    let c = '<div class="link"><a href="#">Roullet</a></div><br> <div class="link"><a href="#">Poker</a></div> <br> <div class="link"><a href="#">Slot</a> </div><br> <div class="link"><a href="#">Coin flip</a> </div><br><div class="link"><a href="#">Plinko</a></div>'
+    let c = '<div class="link"><a href="http://127.0.0.1:5500/games/roullet/index.html">Roullet</a></div><br> <div class="link"> <div class="link"><a href="#">Blackjack</a></div><a href="#">Poker</a></div> <br> <div class="link"><a href="#">Slot</a> </div><br> <div class="link"><a href="#">Coin flip</a> </div><br><div class="link"><a href="#">Plinko</a></div>'
     t.innerHTML= '<button id="tranzicija" ></button> <br>d <br>e<br> s<br> n <br>i <br> <br>n <br>a <br>v <br>' + c
     t.style.textAlign = 'center'
     document.getElementById("tranzicija").addEventListener('click', vrati);
@@ -15,7 +15,7 @@ function pomeranje(){
 function vrati(){
     let x = document.getElementById("desni_nav");
     x.style.width = "100%"
-    let c = '<div class="link"><a href="#">Roullet</a></div><br> <div class="link"><a href="#">Poker</a></div> <br> <div class="link"><a href="#">Slot</a> </div><br> <div class="link"><a href="#">Coin flip</a> </div><br><div class="link"><a href="#">Plinko</a></div>'
+    let c = '<div class="link"><a href="http://127.0.0.1:5500/games/roullet/index.html">Roullet</a></div><br> <div class="link"><a href="#">Blackjack</a></div> <br> <div class="link"><a href="#">Poker</a></div> <br> <div class="link"><a href="#">Slot</a> </div><br> <div class="link"><a href="#">Coin flip</a> </div><br><div class="link"><a href="#">Plinko</a></div>'
     x.innerHTML = '<button id="dugme"></button> <p>desni nav</p>' + c
     document.getElementById("dugme").addEventListener('click', pomeranje);
 }
